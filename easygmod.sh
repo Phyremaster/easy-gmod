d71be34ab73b8db43630b29065bada422e388532
Phyremaster <phyremaster@hotmail.com>
2019-07-12 19:24:09 -0400

    Switched to grep for testing file contents

--- easygmod.sh
+++ easygmod.sh
@@ -12,37 +12,98 @@ cp mount.cfg ${MOUNTCFG}
 
 # Edit server config file
 touch ${SERVERCFG}
-sed -i '/hostname/!{q1}; {s/hostname.*/hostname "'"${HOSTNAME}"'"/}' ${SERVERCFG} || { echo "hostname \"${HOSTNAME}\"" >> ${SERVERCFG}; }
-sed -i '/sv_alltalk/!{q1}; {s/sv_alltalk.*/sv_alltalk '${ALLTALK}'/}' ${SERVERCFG} || { echo "sv_alltalk ${ALLTALK}" >> ${SERVERCFG}; }
-sed -i '/net_maxfilesize/!{q1}; {s/net_maxfilesize.*/net_maxfilesize '${MAXFILESIZE}'/}' ${SERVERCFG} || { echo "net_maxfilesize ${MAXFILESIZE}" >> ${SERVERCFG}; }
+if grep -q 'hostname' ${SERVERCFG}
+then
+    sed -i 's/hostname.*/hostname "'"${HOSTNAME}"'"/' ${SERVERCFG}
+else
+    echo "hostname \"${HOSTNAME}\"" >> ${SERVERCFG}
+fi
+if grep -q 'sv_alltalk' ${SERVERCFG}
+then
+    sed -i 's/sv_alltalk.*/sv_alltalk '${ALLTALK}'/' ${SERVERCFG}
+else
+    echo "sv_alltalk ${ALLTALK}" >> ${SERVERCFG}
+fi
+if grep -q 'net_maxfilesize' ${SERVERCFG}
+then
+    sed -i 's/net_maxfilesize.*/net_maxfilesize '${MAXFILESIZE}'/' ${SERVERCFG}
+else
+    echo "net_maxfilesize ${MAXFILESIZE}" >> ${SERVERCFG}
+fi
 if [ ! -z ${WORKSHOPID} ]
 then
-    sed -i '/host_workshop_collection/!{q1}; {s/host_workshop_collection.*/host_workshop_collection '${WORKSHOPID}'/}' ${SERVERCFG} || { echo "host_workshop_collection ${WORKSHOPID}" >> ${SERVERCFG}; }
+    if grep -q 'host_workshop_collection' ${SERVERCFG}
+    then
+        sed -i 's/host_workshop_collection.*/host_workshop_collection '${WORKSHOPID}'/' ${SERVERCFG}
+    else
+        echo "host_workshop_collection ${WORKSHOPID}" >> ${SERVERCFG}
+    fi
 fi
 if [ ! -z ${DOWNLOADURL} ]
 then
-    sed -i '/sv_downloadurl/!{q1}; {s/sv_downloadurl.*/sv_downloadurl "'${DOWNLOADURL}'"/}' ${SERVERCFG} || { echo "sv_downloadurl \"${DOWNLOADURL}\"" >> ${SERVERCFG}; }
-    sed -i '/sv_allowdownload/!{q1}; {s/sv_allowdownload.*/sv_allowdownload 1/}' ${SERVERCFG} || { echo "sv_allowdownload 1" >> ${SERVERCFG}; }
-    sed -i '/sv_allowupload/!{q1}; {s/sv_allowupload.*/sv_allowupload 1/}' ${SERVERCFG} || { echo "sv_allowupload 1" >> ${SERVERCFG}; }
+    if grep -q 'sv_downloadurl' ${SERVERCFG}
+    then
+        sed -i 's/sv_downloadurl.*/sv_downloadurl "'${DOWNLOADURL}'"/' ${SERVERCFG}
+    else
+        echo "sv_downloadurl \"${DOWNLOADURL}\"" >> ${SERVERCFG}
+    fi
+    if grep -q 'sv_allowdownload' ${SERVERCFG}
+    then
+        sed -i 's/sv_allowdownload.*/sv_allowdownload 1/' ${SERVERCFG}
+    else
+        echo "sv_allowdownload 1" >> ${SERVERCFG}
+    fi
+    if grep -q 'sv_allowupload' ${SERVERCFG}
+    then
+        sed -i 's/sv_allowupload.*/sv_allowupload 1/' ${SERVERCFG}
+    else
+        echo "sv_allowupload 1" >> ${SERVERCFG}
+    fi
 fi
 if [ ! -z ${LOADINGURL} ]
 then
-    sed -i '/sv_loadingurl/!{q1}; {s/sv_loadingurl.*/sv_loadingurl "'${LOADINGURL}'"/}' ${SERVERCFG} || { echo "sv_loadingurl \"${LOADINGURL}\"" >> ${SERVERCFG}; }
+    if grep -q 'sv_loadingurl' ${SERVERCFG}
+    then
+        sed -i 's/sv_loadingurl.*/sv_loadingurl "'${LOADINGURL}'"/' ${SERVERCFG}
+    else
+        echo "sv_loadingurl \"${LOADINGURL}\"" >> ${SERVERCFG}
+    fi
 fi
 if [ ! -z ${PASSWORD} ]
 then
-    sed -i '/sv_password/!{q1}; {s/sv_password.*/sv_password "'${PASSWORD}'"/}' ${SERVERCFG} || { echo "sv_password \"${PASSWORD}\"" >> ${SERVERCFG}; }
+    if grep -q 'sv_password' ${SERVERCFG}
+    then
+        sed -i 's/sv_password.*/sv_password "'${PASSWORD}'"/' ${SERVERCFG}
+    else
+        echo "sv_password \"${PASSWORD}\"" >> ${SERVERCFG}
+    fi
 fi
 if [ ! -z ${RCONPASSWORD} ]
 then
-    sed -i '/rcon_password/!{q1}; {s/rcon_password.*/rcon_password "'${RCONPASSWORD}'"/}' ${SERVERCFG} || { echo "rcon_password \"${RCONPASSWORD}\"" >> ${SERVERCFG}; }
+    if grep -q 'rcon_password' ${SERVERCFG}
+    then
+        sed -i 's/rcon_password.*/rcon_password "'${RCONPASSWORD}'"/' ${SERVERCFG}
+    else
+        echo "rcon_password \"${RCONPASSWORD}\"" >> ${SERVERCFG}
+    fi
 fi
 if [ ! -z ${LOGINTOKEN} ]
 then
-    sed -i '/sv_setsteamaccount/!{q1}; {s/sv_setsteamaccount.*/sv_setsteamaccount "'${LOGINTOKEN}'"/}' ${SERVERCFG} || { echo "sv_setsteamaccount \"${LOGINTOKEN}\"" >> ${SERVERCFG}; }
+    if grep -q 'sv_setsteamaccount' ${SERVERCFG}
+    then
+        sed -i 's/sv_setsteamaccount.*/sv_setsteamaccount "'${LOGINTOKEN}'"/' ${SERVERCFG}
+    else
+        echo "sv_setsteamaccount \"${LOGINTOKEN}\"" >> ${SERVERCFG}
+    fi
 fi
-# sed -i '/exec banned_ip.cfg/!{q1}' ${SERVERCFG} || { echo "exec banned_ip.cfg" >> ${SERVERCFG}; }
-# sed -i '/exec banned_user.cfg/!{q1}' ${SERVERCFG} || { echo "exec banned_user.cfg" >> ${SERVERCFG}; }
+if ! grep -q 'exec banned_ip.cfg' ${SERVERCFG}
+then
+    echo "exec banned_ip.cfg" >> ${SERVERCFG}
+fi
+if ! grep -q 'exec banned_user.cfg' ${SERVERCFG}
+then
+    echo "exec banned_user.cfg" >> ${SERVERCFG}
+fi
 
 # Start the server
 exec ${GMODDIR}/srcds_run -autoupdate -steam_dir ${STEAMCMDDIR} -steamcmd_script /home/steam/autoupdatescript.txt -port 27015 -maxplayers ${MAXPLAYERS} -game garrysmod +gamemode ${GAMEMODE} +map ${GAMEMAP}
